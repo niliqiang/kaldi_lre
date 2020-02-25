7ea707b353e6d9987fde54cf8387ddd9521260e3
niliqiang <ni_liqiang@126.com>
2020-02-25 22:21:34 +0800

    准备语种的set

---
+++ local/data_prep.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+#
+# Copyright 2017   Ewald Enzinger
+# Apache 2.0
+# modified by niliqiang
+#
+# Usage: data_prep.pl /export/data/cv_corpus/zh_CN train out_dir
+
+if (@ARGV != 3) {
+  print STDERR "Usage: $0 <path-to-commonvoice-corpus> <dataset> <path-to-output>\n";
+  print STDERR "e.g. $0 /export/data/cv_corpus/zh_CN train out_dir\n";
+  exit(1);
+}
+
+($db_base, $dataset, $out_dir) = @ARGV;
+
+# 获取语种信息
+$lang = (split("/", $db_base))[-1];
+
+# 依次创建文件夹
+mkdir data unless -d data;
+mkdir "data/$lang" unless -d "data/$lang";
+mkdir $out_dir unless -d $out_dir;
+
+#打开/创建相应文件
+open(META, "<", "$db_base/$dataset.tsv") or die "cannot open dataset TSV file";
+open(UTT2LANG, ">", "$out_dir/utt2lang") or die "Could not open the output file $out_dir/utt2lang";
+open(UTT2SPK, ">", "$out_dir/utt2spk") or die "Could not open the output file $out_dir/utt2spk";
+open(WAV, ">", "$out_dir/wav.scp") or die "Could not open the output file $out_dir/wav.scp";
+
+# 不使用说话人信息，只关注语种信息
+readline META;    # 第一行为标头 skip the first line
+while(<META>) {
+  chomp;
+  ($client_id, $path, $sentence, $up_votes, $down_votes, $age, $gender, $accent) = split(" ", $_);
+  $uttId = $path;
+  $uttId =~ s/\.mp3//g;
+  # No speaker information is provided, so we treat each utterance as coming from a different speaker
+  $spkr = $uttId;
+
+  print WAV "$uttId"," sox $db_base/clips/$path -t wav -r 16k -b 16 -e signed |\n";
+  print UTT2SPK "$uttId"," $spkr","\n";
+  print UTT2LANG "$uttId"," $lang","\n";
+}
+close(META) || die;
+close(UTT2LANG) || die;
+close(UTT2SPK) || die;
+close(WAV) || die;
+
+# =pod  # 块注释，与=cut对应
+# 生成语音和说话人/语种的对应文件
+if (system(
+  "utils/utt2spk_to_spk2utt.pl $out_dir/utt2lang >$out_dir/lang2utt") != 0) {
+  die "Error creating lang2utt file in directory $out_dir";
+}
+if (system(
+  "utils/utt2spk_to_spk2utt.pl $out_dir/utt2spk >$out_dir/spk2utt") != 0) {
+  die "Error creating spk2utt file in directory $out_dir";
+}
+
+# 按行修正和验证
+# LC_COLLATE 定义该环境的排序和比较规则，保证文件是有序的
+system("env LC_COLLATE=C utils/fix_data_dir.sh $out_dir");
+if (system("env LC_COLLATE=C utils/validate_data_dir.sh --no-text --no-feats $out_dir") != 0) {
+  die "Error validating directory $out_dir";
+}
+# =cut
---
+++ run.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Copyright  2014-2015  David Snyder
+#                       Daniel Povey
+# Apache 2.0.
+# modified by niliqiang
+#
+# This script runs the NIST 2007 General Language Recognition Closed-Set
+# evaluation.
+
+# 根据 sre16 v1 以及 lre07 v1 改写，由于 lre07 的代码结构可读性一般，主要根据 sre16 改写
+# 使用common voice数据库
+
+# 包含cmd文件和path文件
+. ./cmd.sh
+. ./path.sh
+# set -e 代表只要出错就停止运行.
+set -e
+
+mfccdir=`pwd`/mfcc
+vaddir=`pwd`/mfcc
+
+#######################################
+# 设置语料存放路径和语料URL
+data=/dataset/cv_corpus
+# 指示系统的执行阶段
+stage=0
+
+if [ $stage -le 0 ]; then
+  # 数据准备（已验证的训练集，开发集，测试集）
+  for part in train dev test; do
+    # 汉语(zh_CN)
+    local/data_prep.pl $data/zh_CN $part data/zh_CN/$part
+	# 土耳其语(tr)
+	local/data_prep.pl $data/tr $part data/tr/$part
+	# 意大利语(it)
+	local/data_prep.pl $data/it $part data/it/$part
+	# 俄语(ru)
+	local/data_prep.pl $data/ru $part data/ru/$part
+	# 爱尔兰语(ru)
+	local/data_prep.pl $data/ga_IE $part data/ga_IE/$part
+  done
+  
+fi
+
+#######################################
+
