3d3eaee2fb20f69fc3e2b7526050a3c46ebf1d55
niliqiang <ni_liqiang@126.com>
2020-03-08 16:19:10 +0800

    添加关于语种数和数据分块数限制的注释以及对应入参的修改

--- run.sh
+++ run.sh
@@ -60,11 +60,11 @@ if [ $stage -le 1 ]; then
   # 计算MFCC、进行端点检测（VAD）
   for part in train dev test; do
     # --cmd 指示：how to run jobs, run.pl或queue.pl
-	# --nj 指示：number of parallel jobs, 默认为4
+	# --nj 指示：number of parallel jobs, 默认为4，需要注意的是nj不能超过说话人数（语种数），以免分割数据的时候被拒绝
 	# 三个目录分别为：数据目录，log目录，mfcc生成目录
-    steps/make_mfcc.sh --cmd "$train_cmd" --nj 20 data/lre/$part exp/make_mfcc/$part $mfccdir
+    steps/make_mfcc.sh --cmd "$train_cmd" --nj 5 data/lre/$part exp/make_mfcc/$part $mfccdir
 	utils/fix_data_dir.sh data/lre/$part
-    steps/compute_vad_decision.sh --cmd "$train_cmd" --nj 20 data/lre/$part exp/make_vad/$part $vaddir
+    steps/compute_vad_decision.sh --cmd "$train_cmd" --nj 5 data/lre/$part exp/make_vad/$part $vaddir
     utils/fix_data_dir.sh data/lre/$part
   done
 fi
@@ -73,14 +73,15 @@ fi
 if [ $stage -le 2 ]; then
   # 使用训练集训练UBM
   # 使用train_diag_ubm.sh脚本的speaker-id版本，二阶动态MFCC，不是SDC，训练一个256的混合高斯
-  sid/train_diag_ubm.sh --cmd "$train_cmd" --nj 20 data/lre/train 256 exp/diag_ubm
+  sid/train_diag_ubm.sh --cmd "$train_cmd" --nj 5 data/lre/train 256 exp/diag_ubm
   # 用先训练的diag_ubm来训练完整的UBM
-  sid/train_full_ubm.sh --cmd "$train_cmd" --nj 20 --remove-low-count-gaussians false data/lre/train exp/diag_ubm exp/full_ubm
+  sid/train_full_ubm.sh --cmd "$train_cmd" --nj 5 --remove-low-count-gaussians false data/lre/train exp/diag_ubm exp/full_ubm
 fi
 
 if [ $stage -le 3 ]; then
-  # 使用训练集训练i-vector提取器，会消耗大量内存，防止程序崩溃，降低nj、num_threads、num_processes（16G内存需要都降到2）
-  sid/train_ivector_extractor.sh --cmd "$train_cmd" --nj 20 --num_threads 2 --num_processes 2 --num-iters 5 exp/full_ubm/final.ubm data/lre/train exp/extractor
+  # 使用训练集训练i-vector提取器，实际运行的线程数是nj*num_processes*num_threads，会消耗大量内存，防止程序崩溃，降低nj、num_threads、num_processes（16G内存需要都降到2）
+  # 还需要注意的是数据分块数为nj*num_processes，这个数据不能超过说话人数（语种数）
+  sid/train_ivector_extractor.sh --cmd "$train_cmd" --nj 5 --num_threads 2 --num_processes 1 --num-iters 5 exp/full_ubm/final.ubm data/lre/train exp/extractor
 fi
 # !
 
@@ -88,7 +89,7 @@ if [ $stage -le 4 ]; then
   # i-vector提取
   for part in train dev test; do
     # 三个目录分别为：i-vector提取器，数据目录，ivectors生成目录
-    sid/extract_ivectors.sh --cmd "$train_cmd" --nj 20 exp/extractor data/lre/$part exp/ivectors_$part
+    sid/extract_ivectors.sh --cmd "$train_cmd" --nj 5 exp/extractor data/lre/$part exp/ivectors_$part
   done
 fi
 
