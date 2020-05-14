43718a0f6cadc8b73b6e1c0d1f6f5b6ad7a5dea4
niliqiang <ni_liqiang@126.com>
2020-05-14 15:11:20 +0800

    cosine scores (To be verified)

--- run.sh
+++ run.sh
@@ -120,7 +120,11 @@ if [ $stage -le 5 ]; then
   # 由于数据库中没有直接的数据来生成trials文件，需要自己组合生成
   # 这个文件是说话人识别特有的，简单来说，就是告诉系统，哪段语音是说话人X说的，哪段语音不是。
   lid/produce_trials.py data/lre/test/utt2lang $trials
+  # 余弦距离打分
+  local/cosine_scoring.sh data/lre/train data/lre/test \
+  exp/ivectors_train exp/ivectors_test $trials exp/scores_cosine_gmm_256
 fi
+
 # !
 
 
