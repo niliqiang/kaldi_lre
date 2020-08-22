7f784e9fde106f06f271e3202cd055e5b3c36752
niliqiang <ni_liqiang@126.com>
2020-08-22 17:46:28 +0800

    prepare for nnet_bottleneck

---
+++ v2/run.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+# Copyright  2014-2015  David Snyder
+#                       Daniel Povey
+#				2020	niliqiang
+# Apache 2.0.
+#
+
+# 包含cmd文件和path文件
+. ./cmd.sh
+. ./path.sh
+# set -e 代表只要出错就停止运行.
+set -e
+
+mfccdir=`pwd`/mfcc
+vaddir=`pwd`/mfcc
+
+# 设置语料存放路径和语料URL
+# 工作站（10.112.212.188）数据集路径
+data_url=www.openslr.org/resources/12
+lm_url=www.openslr.org/resources/11
+data=/mnt/HD1/niliqiang
+
+# 指示系统的执行阶段
+stage=0
+
+if [$stage -le 0 ]
+then
+  # download the data.
+  local/download_and_untar.sh $data $data_url train-clean-100
+  # download the LM resources
+  local/download_lm.sh $lm_url data/local/lm
+
+  # perpare the data and use it to train NN
+  local/data_prep.sh $data/LibriSpeech/train-clean-100 data/$(echo train-clean-100 | sed s/-/_/g)
+fi
+
+if [ $stage -le 1 ]; then
+  # when the "--stage 3" option is used below we skip the G2P steps, and use the
+  # lexicon we have already downloaded from openslr.org/11/
+  local/prepare_dict.sh --stage 3 --nj 30 --cmd "$train_cmd" \
+   data/local/lm data/local/lm data/local/dict_nosp
+
+  utils/prepare_lang.sh data/local/dict_nosp \
+   "<UNK>" data/local/lang_tmp_nosp data/lang_nosp
+
+  local/format_lms.sh --src-dir data/lang_nosp data/local/lm
+fi
+
+if [ $stage -le 2 ]; then
+  # Create ConstArpaLm format language model for full 3-gram and 4-gram LMs
+  utils/build_const_arpa_lm.sh data/local/lm/lm_tglarge.arpa.gz \
+    data/lang_nosp data/lang_nosp_test_tglarge
+  utils/build_const_arpa_lm.sh data/local/lm/lm_fglarge.arpa.gz \
+    data/lang_nosp data/lang_nosp_test_fglarge
+fi
+
+if [$stage -le 3 ]; then
+  steps/make_mfcc_pitch.sh --cmd "$train_cmd" --nj 40 data/train_clean_100 exp/make_mfcc/train_clean_100 $mfccdir
+  steps/compute_cmvn_stats.sh data/train_clean_100 exp/make_mfcc/train_clean_100 $mfccdir
+fi
+
+if [ $stage -le 4 ]; then
+  # Make some small data subsets for early system-build stages.  Note, there are 29k
+  # utterances in the train_clean_100 directory which has 100 hours of data.
+  # For the monophone stages we select the shortest utterances, which should make it
+  # easier to align the data from a flat start.
+  utils/subset_data_dir.sh --shortest data/train_clean_100 2000 data/train_2kshort
+  utils/subset_data_dir.sh data/train_clean_100 5000 data/train_5k
+  utils/subset_data_dir.sh data/train_clean_100 10000 data/train_10k
+fi
+
+f [ $stage -le 5 ]; then
+  # train a monophone system
+  steps/train_mono.sh --boost-silence 1.25 --nj 20 --cmd "$train_cmd" \
+                      data/train_2kshort data/lang_nosp exp/mono
+fi
+
+if [ $stage -le 6 ]; then
+  steps/align_si.sh --boost-silence 1.25 --nj 10 --cmd "$train_cmd" \
+                    data/train_5k data/lang_nosp exp/mono exp/mono_ali_5k
+
+  # train a first delta + delta-delta triphone system on a subset of 5000 utterances
+  steps/train_deltas.sh --boost-silence 1.25 --cmd "$train_cmd" \
+                        2000 10000 data/train_5k data/lang_nosp exp/mono_ali_5k exp/tri1
+fi
+
+if [ $stage -le 7 ]; then
+  steps/align_si.sh --nj 10 --cmd "$train_cmd" \
+                    data/train_10k data/lang_nosp exp/tri1 exp/tri1_ali_10k
+
+
+  # train an LDA+MLLT system.
+  steps/train_lda_mllt.sh --cmd "$train_cmd" \
+                          --splice-opts "--left-context=3 --right-context=3" 2500 15000 \
+                          data/train_10k data/lang_nosp exp/tri1_ali_10k exp/tri2b
+fi
+
+if [ $stage -le 8 ]; then
+  # Align a 10k utts subset using the tri2b model
+  steps/align_si.sh  --nj 10 --cmd "$train_cmd" --use-graphs true \
+                     data/train_10k data/lang_nosp exp/tri2b exp/tri2b_ali_10k
+
+  # Train tri3b, which is LDA+MLLT+SAT on 10k utts
+  steps/train_sat.sh --cmd "$train_cmd" 2500 15000 \
+                     data/train_10k data/lang_nosp exp/tri2b_ali_10k exp/tri3b
+fi
+
+if [ $stage -le 9 ]; then
+  # align the entire train_clean_100 subset using the tri3b model
+  steps/align_fmllr.sh --nj 20 --cmd "$train_cmd" \
+    data/train_clean_100 data/lang_nosp \
+    exp/tri3b exp/tri3b_ali_clean_100
+
+  # train another LDA+MLLT+SAT system on the entire 100 hour subset
+  steps/train_sat.sh  --cmd "$train_cmd" 4200 40000 \
+                      data/train_clean_100 data/lang_nosp \
+                      exp/tri3b_ali_clean_100 exp/tri4b
+fi
+
+if [ $stage -le 10 ]; then
+  # Now we compute the pronunciation and silence probabilities from training data,
+  # and re-create the lang directory.
+  steps/get_prons.sh --cmd "$train_cmd" \
+                     data/train_clean_100 data/lang_nosp exp/tri4b
+  utils/dict_dir_add_pronprobs.sh --max-normalize true \
+                                  data/local/dict_nosp \
+                                  exp/tri4b/pron_counts_nowb.txt exp/tri4b/sil_counts_nowb.txt \
+                                  exp/tri4b/pron_bigram_counts_nowb.txt data/local/dict
+
+  utils/prepare_lang.sh data/local/dict \
+                        "<UNK>" data/local/lang_tmp data/lang
+  local/format_lms.sh --src-dir data/lang data/local/lm
+
+  utils/build_const_arpa_lm.sh \
+    data/local/lm/lm_tglarge.arpa.gz data/lang data/lang_test_tglarge
+  utils/build_const_arpa_lm.sh \
+    data/local/lm/lm_fglarge.arpa.gz data/lang data/lang_test_fglarge
+fi
+
+if [ $stage -le 11 ] && false; then
+  # This stage is for nnet2 training on 100 hours; we're commenting it out
+  # as it's deprecated.
+  # align train_clean_100 using the tri4b model
+  steps/align_fmllr.sh --nj 30 --cmd "$train_cmd" \
+    data/train_clean_100 data/lang exp/tri4b exp/tri4b_ali_clean_100
+
+  # steps/nnet2/train_tanh_bottleneck.sh data/train_clean_100 data/lang exp/tri4b_ali_clean_100 exp/nnet_bottleneck
+fi
+
